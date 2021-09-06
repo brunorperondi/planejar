28ed3f995d8dfeca26646a582dfe18ff7766492f
Bruno <brunorperondi@gmail.com>
2021-09-06 00:03:34 -0300

    Alteração página principal

--- src/app/services/data.service.ts
+++ src/app/services/data.service.ts
@@ -1,3 +1,4 @@
+/* eslint-disable max-len */
 import { Injectable } from '@angular/core';
 
 export interface Message {
@@ -6,6 +7,10 @@ export interface Message {
   date: string;
   id: number;
   read: boolean;
+  message: string;
+  airTicket: string;
+  accommodation: string;
+  locomotion: string;
 }
 
 @Injectable({
@@ -14,60 +19,26 @@ export interface Message {
 export class DataService {
   public messages: Message[] = [
     {
-      fromName: 'Matt Chorsey',
-      subject: 'New event: Trip to Vegas',
+      fromName: 'Rock In Rio',
+      subject: 'Data Evento: 2022',
       date: '9:32 AM',
       id: 0,
-      read: false
+      read: false,
+      message: 'O Rock in Rio é um festival de música idealizado pelo empresário brasileiro Roberto Medina pela primeira vez em 1985, sendo, desde sua criação, reconhecidamente, o maior festival musical do planeta. Foi originalmente organizado no Rio de Janeiro, de onde vem o nome.',
+      airTicket: 'LATAM',
+      accommodation: 'AIRBNB',
+      locomotion: 'UBER'
     },
     {
-      fromName: 'Lauren Ruthford',
-      subject: 'Long time no chat',
+      fromName: 'KnotFest',
+      subject: 'Data Evento: nov/2022',
       date: '6:12 AM',
       id: 1,
-      read: false
-    },
-    {
-      fromName: 'Jordan Firth',
-      subject: 'Report Results',
-      date: '4:55 AM',
-      id: 2,
-      read: false
-    },
-    {
-      fromName: 'Bill Thomas',
-      subject: 'The situation',
-      date: 'Yesterday',
-      id: 3,
-      read: false
-    },
-    {
-      fromName: 'Joanne Pollan',
-      subject: 'Updated invitation: Swim lessons',
-      date: 'Yesterday',
-      id: 4,
-      read: false
-    },
-    {
-      fromName: 'Andrea Cornerston',
-      subject: 'Last minute ask',
-      date: 'Yesterday',
-      id: 5,
-      read: false
-    },
-    {
-      fromName: 'Moe Chamont',
-      subject: 'Family Calendar - Version 1',
-      date: 'Last Week',
-      id: 6,
-      read: false
-    },
-    {
-      fromName: 'Kelly Richardson',
-      subject: 'Placeholder Headhots',
-      date: 'Last Week',
-      id: 7,
-      read: false
+      read: false,
+      message: 'Conhecido por ser um dos maiores festivais de metal do mundo, a edição brasileira terá como headliner os mascarados do Slipknot, donos do evento. Um dos grandes atrativos é o Mr. Bungle, projeto do vocalista Mike Patton (Faith No More), que retornou as atividades após 20 anos de hiato e tocará no país pela primeira vez. Já o Trivium e o Bring Me The Horizon são conhecidos - e sempre muito aguardados - pelos fãs.',
+      airTicket: 'Azul',
+      accommodation: 'AIRBNB',
+      locomotion: 'Alugar Carro'
     }
   ];
 
