633440e7271c20046b09bec687922b40610682ab
Bruno <brunorperondi@gmail.com>
2021-09-16 01:20:14 -0300

    Página de Sign up e Log in

---
+++ src/app/components/input/input.module.ts
@@ -0,0 +1,12 @@
+import { InputComponent } from './input.component';
+import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
+import { IonicModule } from '@ionic/angular';
+
+@NgModule({
+  declarations: [InputComponent],
+  imports: [CommonModule, IonicModule],
+  exports: [InputComponent]
+})
+
+export class InputModule {}
