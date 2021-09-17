9273f614b1c93df285879856a6b87a32a22d2657
Bruno <brunorperondi@gmail.com>
2021-09-17 00:27:36 -0300

    Mudanças no App

--- src/app/services/data.service.ts
+++ src/app/services/data.service.ts
@@ -3,7 +3,6 @@ import { Injectable } from '@angular/core';
 
 export interface Message {
   fromName: string;
-  subject: string;
   date: string;
   id: number;
   read: boolean;
@@ -11,6 +10,7 @@ export interface Message {
   airTicket: string;
   accommodation: string;
   locomotion: string;
+  situation: string;
 }
 
 @Injectable({
@@ -20,25 +20,25 @@ export class DataService {
   public messages: Message[] = [
     {
       fromName: 'Rock In Rio',
-      subject: '2022',
-      date: '9:32 AM',
+      date: '2022',
       id: 0,
       read: false,
       message: 'O Rock in Rio é um festival de música idealizado pelo empresário brasileiro Roberto Medina pela primeira vez em 1985, sendo, desde sua criação, reconhecidamente, o maior festival musical do planeta. Foi originalmente organizado no Rio de Janeiro, de onde vem o nome.',
-      airTicket: 'LATAM',
-      accommodation: 'AIRBNB',
-      locomotion: 'UBER'
+      airTicket: 'Latam',
+      accommodation: 'Airbnb',
+      locomotion: 'Uber',
+      situation: 'Executar',
     },
     {
       fromName: 'KnotFest',
-      subject: 'dez/2022',
-      date: '6:12 AM',
+      date: '2022',
       id: 1,
       read: false,
       message: 'Conhecido por ser um dos maiores festivais de metal do mundo, a edição brasileira terá como headliner os mascarados do Slipknot, donos do evento. Um dos grandes atrativos é o Mr. Bungle, projeto do vocalista Mike Patton (Faith No More), que retornou as atividades após 20 anos de hiato e tocará no país pela primeira vez. Já o Trivium e o Bring Me The Horizon são conhecidos - e sempre muito aguardados - pelos fãs.',
       airTicket: 'Azul',
-      accommodation: 'AIRBNB',
-      locomotion: 'Alugar Carro'
+      accommodation: 'Airbnb',
+      locomotion: 'Alugar',
+      situation: 'Executar',
     }
   ];
 
